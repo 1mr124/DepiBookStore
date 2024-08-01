1cba589a900059c38577f4cd8a87c23b2b49a7a0
Zeina Seif <zeinaseif760@gmail.com>
2024-08-01 16:11:11 +0300

    version2

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+
+//Fun
+function loadHTML(elementID, filePath) {
+  fetch(filePath)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(elementID).innerHTML = data;
+    });
+}
+
+loadHTML("nav-placeholder", "nav.html");
+loadHTML("footer-placeholder", "footer.html");
+
+
+
